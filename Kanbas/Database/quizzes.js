bae8d1981433d898f03bb236295ec89e79ce7cab
JarodD19 <jaroddes19@gmail.com>
2024-04-19 07:26:39 -0400

    Quiz details screen done

--- Kanbas/Database/quizzes.js
+++ Kanbas/Database/quizzes.js
@@ -2,23 +2,78 @@ export default [
     {
         "id": "Q1",
         "course": "RS101",
-        "name": "Quiz 1",
-        "available_date": "2024-04-15",
-        "due_date": "2024-04-20",
-        "points": "10",
-        "num_questions": "5",
-        "time_due": "11:59 PM",
-        "published": false
+        "name": "End of Module Graded Quiz",
+        "available_date": "2024-04-25T08:00:00Z",
+        "due_date": "2024-05-02T23:59:00Z",
+        "type": "Graded Quiz",
+        "points": 50,
+        "group": "Quizzes",
+        "shuffle": true,
+        "time_limit": 20,
+        "repeat": false,
+        "answer_key": false,
+        "access_code": "",
+        "one_at_a_time": true,
+        "webcam": false,
+        "locked_after_answering": false,
+        "time_due": "2024-05-02T23:59:00Z",
+        "published": true,
+        "questions": [
+            {
+                "question": "What is the capital of France?",
+                "points": 10,
+                "type": "multiple-choice"
+            },
+            {
+                "question": "Calculate the force needed...",
+                "points": 20,
+                "type": "numerical"
+            },
+            {
+                "question": "Describe the process of photosynthesis.",
+                "points": 20,
+                "type": "essay"
+            }
+        ]
+
     },
     {
         "id": "Q2",
         "course": "RS101",
         "name": "Test",
-        "available_date": "2024-04-15",
-        "due_date": "2024-04-20",
-        "points": "10",
-        "num_questions": "5",
-        "time_due": "11:59 PM",
-        "published": false
+        "name": "Weekly Practice Quiz",
+        "available_date": "2024-04-26T08:00:00Z",
+        "due_date": "2024-05-03T23:59:00Z",
+        "type": "Practice Quiz",
+        "points": 30,
+        "group": "Quizzes",
+        "shuffle": true,
+        "time_limit": 20,
+        "repeat": true,
+        "answer_key": true,
+        "access_code": "1234",
+        "one_at_a_time": true,
+        "webcam": false,
+        "locked_after_answering": false,
+        "time_due": "2024-05-03T23:59:00Z",
+        "published": true,
+        "questions": [
+            {
+                "question": "Which element has the symbol 'Na'?",
+                "points": 10,
+                "type": "multiple-choice"
+            },
+            {
+                "question": "What is the square root of 144?",
+                "points": 10,
+                "type": "numerical"
+            },
+            {
+                "question": "Name three types of clouds.",
+                "points": 10,
+                "type": "short-answer"
+            }
+        ]
     }
+
 ]
--- Kanbas/quizzes/routes.js
+++ Kanbas/quizzes/routes.js
@@ -24,8 +24,15 @@ export default function QuizRoutes(app) {
         res.json(result);
     };
 
+    const findQuizById = async (req, res) => {
+        const quiz = await dao.findQuizById(req.params.qid);
+        res.json(quiz);
+    }
+
     app.delete("/api/quizzes/:qid", deleteQuiz);
     app.post("/api/courses/:cid/quizzes", createQuiz);
     app.put("/api/quizzes/:qid", updateQuiz);
+    app.get("/api/quizzes/:qid",findQuizById);
     app.get("/api/courses/:cid/quizzes", findQuizzesForCourse);
+    
 }
