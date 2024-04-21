377ad8c392f73a4f46aa7dae3ca427b6a64fa8ff
JarodD19 <jaroddes19@gmail.com>
2024-04-21 02:59:09 -0400

    Ready to deploy

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import ModuleRoutes from './Kanbas/modules/routes.js';
 import QuizRoutes from './Kanbas/quizzes/routes.js';
 import "dotenv/config";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors({
